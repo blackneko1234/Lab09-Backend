46e3ca3233e92584887de60ab8ad63c59c1c000d
Decha_L <blackneko1234@hotmail.com>
2021-09-09 10:31:12 +0700

    3.6
    3.6

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.service.EventService;
+import se331.lab.rest.util.LabMapper;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -26,7 +27,8 @@ public class EventController {
         Page<Event> pageOutput = eventService.getEvents(perPage, page);
         HttpHeaders responseHeader = new HttpHeaders();
         responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
-        return new ResponseEntity<>(pageOutput.getContent(),responseHeader,HttpStatus.OK);
+        //return new ResponseEntity<>(pageOutput.getContent(),responseHeader,HttpStatus.OK);
+        return new ResponseEntity<>(LabMapper.INSTANCE.getEventDto(pageOutput.getContent()), responseHeader, HttpStatus.OK);
 
     }
 
@@ -35,17 +37,18 @@ public class EventController {
 
         Event output = eventService.getEvent(id);
         if (output != null) {
-            return ResponseEntity.ok(output);
+            //return ResponseEntity.ok(output);
+            return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
     }
 
     @PostMapping("/events")
-    public ResponseEntity<?> addEvent(@RequestBody Event event){
+    public ResponseEntity<?> addEvent(@RequestBody Event event) {
         Event output = eventService.save(event);
-        return ResponseEntity.ok(event);
-
+        //return ResponseEntity.ok(event);
+        return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
 
     }
 }
